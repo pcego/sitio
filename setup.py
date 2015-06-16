5270274a18958cb749490df2eb5a8982fb9d9ef7
gpzim98 <gpzim98@gmail.com>
2015-06-15 22:27:03 -0300

    First commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='django 1.o on Red Hat Openshift',
+    version='1.3',
+    description='django on OpenShift',
+    author='Gregory Pacheco',
+    author_email='contato@gregorypacheco.com.br',
+    url='https://github.com/Gpzim98/Django-OpenShift-1.8.git',
+)
