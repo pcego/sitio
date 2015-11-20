b5d58e4e03f4c0b6a0d108c5896635c36383790e
pcego <pcego36@gmail.com>
2015-11-20 21:00:58 -0200

    add requiriment on setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='Django On OpenShift with Django 1.8',
+setup(name='recanto',
     version='0.1',
-    description='django on OpenShift',
-    author='Gregory Pacheco',
-    author_email='contato@gregorypacheco.com.br',
-    url='https://github.com/Gpzim98/Django-OpenShift-1.8.git',
+    description='Site Chacara Recanto da Paz',
+    author='pcego',
+    author_email='pcego36@gmail.com',
+    url='',
     install_requires=['Django==1.8.2',],
 )
