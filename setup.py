74e44ba7aec0437e65882a89ff480897eb1bcc7b
pcego <pcego36@gmail.com>
2015-11-20 20:49:35 -0200

    mod setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='Recanto',
       version='1.0',
       description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      author='Paulo',
+      author_email='pcego36@gmail.com	',
       url='http://www.python.org/sigs/distutils-sig/',
 #      install_requires=['Django>=1.3'],
      )
