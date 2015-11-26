0aace4bd4f67c3917b095b8b15c7c524404fccdf
pcego <pcego36@gmail.com>
2015-11-25 22:16:46 -0200

    add models

--- sitio/models.py
+++ sitio/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Album(models.Model):
+	nome = models.CharField(max_length = 200)
+	data_criacao = models.DateField()
+
+class Foto(models.Model):
+	imagem = models.ImageField(upload_to='static/media/', blank=False,null=False)
+	    
