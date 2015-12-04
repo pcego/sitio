50ad59e4230e8ce620668a879c94b4e6952d4d45
pcego <pcego36@gmail.com>
2015-12-03 23:01:38 -0200

    adding new template gallery

--- project/urls.py
+++ project/urls.py
@@ -2,6 +2,13 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', 'project.views.home', name='url_home'),
-    url(r'^admin/', include(admin.site.urls)),    
+    url(r'^admin/', include(admin.site.urls)), 	
+    url(r'^$', 'project.views.home', name='url_home'),    
+    url(r'^fotos/piscina$', 'project.views.piscina', name = 'url_fotos_piscina'), 
+    url(r'^fotos/chales$', 'project.views.chale', name = 'url_fotos_chales'), 
+    url(r'^fotos/cozinha$', 'project.views.cozinha', name = 'url_fotos_cozinha'), 
+    url(r'^fotos/churrasqueira$', 'project.views.churrasqueira', name = 'url_fotos_churrasqueira'), 
+    url(r'^fotos/geral$', 'project.views.geral', name = 'url_fotos_geral'), 
+    url(r'^fotos/esportes$', 'project.views.esporte', name = 'url_fotos_esportes'), 
+
 ]
--- project/views.py
+++ project/views.py
@@ -4,3 +4,21 @@ from django.shortcuts import render
 
 def home(request):
 	return render(request, 'index.html')
+
+def piscina(request):
+	return render(request, 'gallery.html')
+
+def chale(request):
+	return HttpResponse('teste chales')
+
+def cozinha(request):
+	return HttpResponse('teste cozinha')
+
+def geral(request):
+	return HttpResponse('teste geral')
+
+def churrasqueira(request):
+	return HttpResponse('teste churrasqueira')
+
+def esporte(request):
+	return HttpResponse('teste esportes')
